e397aba59977f1efb2e0a7310cd8cc59eeef68f5
Wassauf <Wassaufk@gmail.com>
2020-09-10 20:34:03 +0500

    Added script for password generation and modified readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Password_Generator
+
+#### Script to generate random and secure passwords containing upper and lowercase letters, symbols and numbers.
---
+++ password_generator.py
@@ -0,0 +1,26 @@
+import string
+import random
+
+def generate_pass():
+    digits_list = list(string.digits)
+    symbols = list(string.punctuation)
+    cap_aplha = list(string.ascii_uppercase)
+    small_aplha = list(string.ascii_lowercase)
+    master_list = digits_list + symbols + cap_aplha + small_aplha
+    # print(master_list)
+
+    password = ""
+    password_size = random.randint(12,18)
+    for i in range(password_size):
+        password += random.choice(master_list)
+    
+    return password
+
+if __name__ == "__main__":
+    print("-----------------------------------------------------\nIntelligent Password Generator of VARIABLE LENGTH with\nMIXED UPPER CASE, LOWER CASE characters and SYMBOLS\n-----------------------------------------------------\n ")
+    print(generate_pass())
+
+# characters = string.ascii_letters + string.punctuation  + string.digits
+# password =  "".join(choice(characters) for x in range(randint(8, 16)))
+# print (password)
+
